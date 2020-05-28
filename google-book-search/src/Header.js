3d09974fbb30849ede24d5601e9f4f4b0fc770d6
lokisixonenine <jaycee.bagtas@gmail.com>
2020-05-27 20:36:33 -0700

    created react app

---
+++ google-book-search/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from './App';
+import './App.css';
+
+const Header =  () => {
+    return (
+        <header>
+            <i class="fas fa-book-open fa-3x"></i>
+            <h1>Google Book Search</h1>
+        </header>        
+    )
+}
+
+export default Header;
